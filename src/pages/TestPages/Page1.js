d207843565bed62fa7290f552726ff6adb99264c
OwenG50 <opgeis@gmail.com>
2024-01-29 12:28:22 -0500

    Update Page1.js
    test

--- src/pages/TestPages/Page1.js
+++ src/pages/TestPages/Page1.js
@@ -5,6 +5,7 @@ export function Page1(){
     return(
       <div>
           <h1>Page 1 (Home Page)</h1>
+          <h2>Test for hosting</h2>
           <SignInWithMicrosoft></SignInWithMicrosoft>
       </div>
     );
