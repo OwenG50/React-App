06d7217fb9ee420e473f6c51479274d5ba529a62
OwenG50 <opgeis@gmail.com>
2024-04-19 15:46:04 -0400

    Update WS_Scan_Results.js

--- src/components/Work Study/WS_Scan_Results.js
+++ src/components/Work Study/WS_Scan_Results.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function WS_Scan_Results(props) {
 
     const QrData = sessionStorage.getItem('QrData');
+    const [reload, setReload] = useState(false); // State to trigger re-render
 
     //console.log(QrData);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setReload(prev => !prev); // Toggle the state to force re-render
+        }, 1000); // Set the timer for 1 second
+
+        return () => clearTimeout(timer); // Clean up the timer
+    }, [reload]); // Depend on `reload` to reset the timer if needed
 
     return (
         <div>
